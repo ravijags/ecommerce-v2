065f24a008f3cbba5643dc28aad421738e3248e3
ravijags <rvjags007@gmail.com>
2026-04-07 12:15:18 +0530

    initial setup - server and user model

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Failed",err.message)
+  
+ );
+
+ const PORT = process.env.PORT || 3000;
+
+ app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    
+ });
