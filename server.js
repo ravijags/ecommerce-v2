48e9dcfa8097ec2aed68f7240f5003321c5ac1bb
ravijags <rvjags007@gmail.com>
2026-05-04 16:06:49 +0530

    add security - helmet and cors

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+// const hpp = require("hpp");
 const { generalLimiter } = require("./middleware/rateLimiter");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const errorHandler = require("./middleware/errorMiddleware");
+
 dotenv.config();
 const app = express();
+
+app.use(helmet());
+app.use(cors());
+//app.use(mongoSanitize());
+// app.use(hpp());
 
 app.use(express.json());
 app.use(generalLimiter);
@@ -15,11 +30,7 @@ mongoose
   
  );
 
- const authRoutes = require("./routes/authRoutes");
- const productRoutes = require("./routes/productRoutes");
- const cartRoutes = require("./routes/cartRoutes");
- const orderRoutes = require("./routes/orderRoutes");
- const errorHandler = require("./middleware/errorMiddleware");
+ 
 
  app.use("/api/auth", authRoutes);
  app.use("/api/products", productRoutes);
